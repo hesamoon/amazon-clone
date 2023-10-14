ba5ae4fffb12c63d3abf8284010812a8d1442e79
hesamoon <he3am.jsm@gmail.com>
2023-10-14 14:42:36 +0330

    edit home style mb for home__image class

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className='home'>
       <div className='home__container'>
-        {/* <img className='home__image' src={bg} alt=''/> */}
+        <img className='home__image' src="https://m.media-amazon.com/images/I/71U-Q+N7PXL._SX3000_.jpg" alt=''/>
 
         <div className='home__row'>
           <Product 
